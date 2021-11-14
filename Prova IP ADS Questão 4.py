b5ebed0c3034ccdab3230c80ccdb711162a0628a
Ewerton-Pedrosa <ewertoon.felipe@gmail.com>
2021-11-14 11:09:40 -0300

    coments

--- Prova IP ADS Questão 4.py
+++ Prova IP ADS Questão 4.py
@@ -8,8 +8,9 @@ while True:
     notas.append(nota)
     cont += 1
 
-print(notas)
-print(len(notas))
-print(sum(notas))
-print((sum(notas)/len(notas)))
+print(f'Notas na ordem de inserção: {notas}') # Mostra notas na Ordem de entrada
+print(len(notas)) # Mostra quantas notas foram inseridas
+print(sum(notas)) # Mostra a soma das notas
+print((sum(notas)/len(notas))) # Mostra a média das notas
+
   
