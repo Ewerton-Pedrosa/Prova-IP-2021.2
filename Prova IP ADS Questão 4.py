86a333b18530a86368d9b4fdeca0ef95c738de2d
Ewerton-Pedrosa <ewertoon.felipe@gmail.com>
2021-11-15 08:48:33 -0300

    Questão 4 Completa usando somente biblioteca math (se quiser posso usar somente operadores lógicos por exemplo para exponenciação usar **)

--- Prova IP ADS Questão 4.py
+++ Prova IP ADS Questão 4.py
@@ -1,6 +1,12 @@
+import math
+
+# DECLARANDO VARIÁVEIS
 cont = 1
 notas = []
+notasAcimaMedia = []
+distanciaAteMedia = []
 
+# LOOP DE ENTRADA DE DADOS
 while True:
     nota = int(input(f'Digite a nota {cont}: '))
     if nota < 0:
@@ -8,9 +14,25 @@ while True:
     notas.append(nota)
     cont += 1
 
-print(f'Notas na ordem de inserção: {notas}') # Mostra notas na Ordem de entrada
-print(len(notas)) # Mostra quantas notas foram inseridas
-print(sum(notas)) # Mostra a soma das notas
-print((sum(notas)/len(notas))) # Mostra a média das notas
+# CALCULA NOTAS PARA EXIBIR NA TELA FINAL
+media = (sum(notas)/len(notas))
+qtNotas = len(notas)
+somaNotas = sum(notas)
+for i in range(len(notas)):
+    if notas[i] > media:
+        notasAcimaMedia.append(notas[i])
 
-  
+# CALCULO MANUAL DO DESVIO PADRÃO
+for i in range(len(notas)):
+    distanciaAteMedia.append(math.pow(notas[i]-media, 2))     # Cria lista com distancias entre elementos e média
+somaDistancias = sum(distanciaAteMedia) # Soma todos os elementos da lista "distanciaAteMedia"
+somatorioDivQtNotas = somaDistancias/qtNotas # Somatorio dividido pela quantidade de notas
+desvioPadrao = math.sqrt(somatorioDivQtNotas) # Uso biblioteca Math para calcular Raiz Quadrada
+
+# EXIBIR RESULTADOS
+print(f'Notas na ordem de inserção: {notas}') # Mostra notas na Ordem de entrada
+print(qtNotas) # Mostra quantas notas foram inseridas
+print(somaNotas) # Mostra a soma das notas
+print(media) # Mostra a média das notas
+print(notasAcimaMedia) # Lista com notas acima da média
+print(desvioPadrao)
